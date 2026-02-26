f263c3ab2d146e23aac30e19ea5f7fc922da26f6
fakhryhizballah <falehry88@gmail.com>
2026-02-27 00:21:09 +0700

    handel errors

--- Public/js/audio.js
+++ Public/js/audio.js
@@ -16,7 +16,7 @@ function playAudiosSequentially(audioPaths) {
             playAudiosSequentially(); // Lanjutkan ke audio berikutnya setelah selesai
         };
     } catch (error) {
-
+        sedangProses = false;
     }
 
 }
